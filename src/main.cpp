bfb407ac5f40fa9c813666eb045adddd1a15434d
Yilard <40509594+Yilard@users.noreply.github.com>
2021-10-03 23:56:10 +1100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include "TermiCube.h"
 
+
 int main() {
 
     std::cout << "[ Game Started ]\n\n";
 
-    Game game {};
+    GameWindow window;
 
-    while (game.getState() != GameState::END) {
-        game.userInput();
-        game.updateGame();
-        game.drawGraphics();
+    while (!window.gameEnded()) {
+        window.pollEvents();
+        window.updateWindow();
     }
-    
+
     std::cout << "\n[ Game Ended ]" << std::endl;
 
     return 0;
