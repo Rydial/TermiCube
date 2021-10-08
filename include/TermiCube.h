f599a5798780a10451c55c574766ed77bc42d4b7
Yilard <40509594+Yilard@users.noreply.github.com>
2021-10-08 15:12:09 +1100

    Update TermiCube.h

--- include/TermiCube.h
+++ include/TermiCube.h
@@ -1,14 +1,16 @@
 #ifndef TERMICUBE_H
 #define TERMICUBE_H
 
+// #define _XOPEN_SOURCE_EXTENDED
 
 #include <vector>
 #include <memory>
+#include <iostream>
 #include <panel.h>
 
-
 enum class ScreenType {
-    MAINMENU, /*SETTINGS,*/ GAME, /*INVENTORY*/
+    /* Order must follow initScreens() emplace_back order */
+    MAINMENU
 };
 
 
@@ -18,13 +20,11 @@ class Screen {
     protected:
         /* Need a custom deleter function to use unique_ptr with an incomplete type */
         /* Later on replace this with a pimpl-idiom */
-        struct PanelDeleter {
-            void operator()(PANEL *ptr) {del_panel(ptr);}
-        };
+        struct PanelDeleter {void operator()(PANEL *ptr);};
+        struct SubWindowDeleter {void operator()(WINDOW *ptr);};
         std::unique_ptr<PANEL, PanelDeleter> panel;
-        static constexpr int rows {35}, cols {70};
+        static constexpr int rows {40}, cols {80};
     public:
-        /* all four 0 means full screen */
         Screen();
         virtual ~Screen() = default;
         virtual void drawGraphics() = 0;
@@ -33,9 +33,10 @@ class Screen {
 };
 
 class MainMenuScreen : public Screen {
+    private:
+        
     public:
-        /* Inherit Constructor from Screen */
-        using Screen::Screen;
+        MainMenuScreen();
         void drawGraphics();
         void updateScreen();
         void userInput();
@@ -43,6 +44,7 @@ class MainMenuScreen : public Screen {
 
 class GameScreen : public Screen {
     public:
+        /* Inherit Constructor from Screen */
         using Screen::Screen;
         void drawGraphics();
         void updateScreen();
@@ -60,18 +62,10 @@ class GameWindow {
         void initCurses();
         void initScreens();
     public:
-        GameWindow() :
-            screenList{},
-            screen{ScreenType::MAINMENU},
-            exit{false}
-        {
-            initCurses();
-            initScreens();
-        }
+        GameWindow();
         bool gameEnded() const {return exit;}
-        void pollEvents();
         void terminate() {exit = true;}
-        void updateWindow();
+        void update();
 };
 
 
