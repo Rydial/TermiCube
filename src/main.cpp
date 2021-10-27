86f7f10a736afe95d62996bb68ee972e137af916
Danny <kimdanny2001@gmail.com>
2021-10-27 23:09:37 +1100

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,46 @@
 #include "TermiCube.h"
 
 
-int main() {
+// int main() {
 
-    GameWindow game;
+//     GameWindow game;
 
-    while(game.update() == 0);
+//     while(game.update() == 0);
 
-    return 0;
-}
+//     return 0;
+// }
   
 
+#include <functional>
+#include <iostream>
 
-// #include <iostream>
-// #include <string>
-// #include <cstring>
-// #include <bitset>
-// #include <fstream>
- 
-// int main()
-// {
-//     setlocale(LC_ALL, "");
-//     // UTF-8 narrow multibyte encoding
-//     std::string path {"resource/mainmenu/NewGameBtn.txt"};
-//     std::ifstream file {path};
-//     std::string line;
+class Game;
+static std::function<void(Game &)> func;
+static int *ref;
 
-//     if (!file)
-//         std::cerr << "File could not be opened: " << path << '\n';
+class Game {
+    private:
+        int screen;
+        void foo()
+        {
+            screen = 2;
+        }
+    public:
+        Game() :
+            screen{1}
+        {
+            ref = {&screen};
+            func = {&Game::foo};
+        }
+        void print() {
+            std::cout << screen << '\n';
+        }
+};
 
-//     while (std::getline(file, line)) {
-//         std::cout << line << '\n';
-//         size_t space {0}, count {0};
-
-//         for (size_t i {0}; i < line.length(); i++) {
-//             if (line[i] == ' ')
-//                 space++;
-//             /* In Windows a newline is represented in the CR + LF format: "\r\n" */
-//             else if (line[i] != '\r') {
-//                 std::bitset<8> byte {static_cast<unsigned long long>(line[i])};
-//                 size_t bits {0};
-
-//                 for (size_t j {0}; j < 8 && byte.test(7 - j); j++, bits++);
-
-//                 if (bits == 3 || bits == 4) {
-//                     i += bits - 1;
-//                 } else if (bits == 2) {
-//                     std::bitset<8> nextByte {static_cast<unsigned long long>(line[++i])};
-
-//                     if (!nextByte.test(7) || nextByte.test(6)) {
-//                         std::cerr << "No continuation byte found\n";
-//                         exit(1);
-//                     }
-//                 }
-//                 count++;
-//             }
-//         }
-//         std::cout << "\nSpaces : " << space << '\n';
-//         std::cout << "\nUnicode Chars : " << count << "\n\n";
-//     }
-// }
+int main() {
+    Game game;
+    func(game);
+    game.print();
+    *ref = 5;
+    game.print();
+}
